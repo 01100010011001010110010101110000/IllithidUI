59d0f345f888b4aea71950bc48433cdd2d7c7378
Tyler Gregory <td.g@icloud.com>
2019-06-05 08:45:08 -0500

    Initial Commit

---
+++ IllithidUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  IllithidUI
+//
+//  Created by Tyler Gregory on 6/5/19.
+//  Copyright © 2019 Tyler Gregory. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
