29ba7416d3a390f7248a8c810433c49902f4889e
Tyler Gregory <tdgregory@protonmail.com>
2019-12-03 22:08:34 -0600

    Handle the case when `postHint` is nil

--- IllithidUI/Post/PostPreview.swift
+++ IllithidUI/Post/PostPreview.swift
@@ -15,32 +15,34 @@ struct PostPreview: View {
 
   var body: some View {
     return VStack {
-      if post.postHint == nil {
-        EmptyView()
-      } else {
-        if post.postHint! == .`self` {
-          Text(post.selftext)
-        } else if post.postHint! == .hostedVideo || post.postHint! == .richVideo {
-          if post.preview?.redditVideoPreview?.scrubberMediaUrl != nil {
-            Player(url: post.preview!.redditVideoPreview!.hlsUrl)
-              .frame(width: CGFloat(post.preview!.redditVideoPreview!.width),
-                     height: CGFloat(post.preview!.redditVideoPreview!.height))
-          } else {
-            LinkPreview(link: post.contentUrl)
-              .fixedSize(horizontal: true, vertical: false)
-          }
-        } else if post.postHint! == .image {
-          if !post.previews.isEmpty {
-            RemoteImage(post.previews.middle!.url)
-              .frame(width: CGFloat(integerLiteral: post.previews.middle!.width),
-                     height: CGFloat(integerLiteral: post.previews.middle!.height))
-          } else {
-            Image(nsImage: NSImage(imageLiteralResourceName: "NSUser"))
-              .scaledToFit()
-              .frame(width: 96, height: 96)
-          }
+      if post.postHint == .`self` {
+        Text(post.selftext)
+      } else if post.postHint == .hostedVideo || post.postHint == .richVideo {
+        if post.preview?.redditVideoPreview?.scrubberMediaUrl != nil {
+          Player(url: post.preview!.redditVideoPreview!.hlsUrl)
+            .frame(width: CGFloat(post.preview!.redditVideoPreview!.width),
+                   height: CGFloat(post.preview!.redditVideoPreview!.height))
+        }
+      } else if post.postHint == .image {
+        if !post.previews.isEmpty {
+          RemoteImage(post.previews.middle!.url)
+            .frame(width: CGFloat(integerLiteral: post.previews.middle!.width),
+                   height: CGFloat(integerLiteral: post.previews.middle!.height))
         } else {
-          Text("I didn't have a post hint")
+          Image(nsImage: NSImage(imageLiteralResourceName: "NSUser"))
+            .scaledToFit()
+            .frame(width: 96, height: 96)
+        }
+      } else if post.postHint == .link {
+        LinkPreview(link: post.contentUrl)
+          .fixedSize(horizontal: true, vertical: false)
+      } else {
+        // There was no post hint or it did not match any prior case
+        if !post.selftext.isEmpty {
+          Text(post.selftext)
+        } else {
+          LinkPreview(link: post.contentUrl)
+            .fixedSize(horizontal: true, vertical: false)
         }
       }
     }
