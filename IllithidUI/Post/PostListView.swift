191048557c7168243f00f2d57bbea17da77bee23
Tyler Gregory <tdgregory@protonmail.com>
2020-02-16 13:45:40 -0600

    Add initial sidebar support

--- IllithidUI/Post/PostListView.swift
+++ IllithidUI/Post/PostListView.swift
@@ -9,10 +9,12 @@ import Foundation
 import SwiftUI
 
 import Illithid
+import SDWebImageSwiftUI
 
 struct PostListView<PostContainer: PostsProvider>: View {
   @ObservedObject var postsData: PostData<PostContainer>
   @State private var searchText: String = ""
+  @State private var showSidebar: Bool = false
 
   let postContainer: PostContainer
   let commentsManager: WindowManager = WindowManager<CommentsView>()
@@ -21,9 +23,9 @@ struct PostListView<PostContainer: PostsProvider>: View {
   private let cancelToken: AnyCancellable? = nil
 
   private var filteredPosts: [Post] {
-    self.postsData.posts.filter {
-      if searchText.isEmpty { return true }
-      return $0.author.range(of: searchText, options: .diacriticInsensitive) != nil ||
+    if searchText.isEmpty { return postsData.posts }
+    return postsData.posts.filter {
+      $0.author.range(of: searchText, options: .diacriticInsensitive) != nil ||
         $0.title.range(of: searchText, options: .diacriticInsensitive) != nil ||
         $0.subreddit.range(of: searchText, options: .diacriticInsensitive) != nil ||
         $0.selftext.range(of: searchText, options: .diacriticInsensitive) != nil
@@ -32,7 +34,7 @@ struct PostListView<PostContainer: PostsProvider>: View {
 
   init(postContainer: PostContainer) {
     self.postContainer = postContainer
-    self.postsData = PostData(provider: self.postContainer)
+    postsData = PostData(provider: self.postContainer)
   }
 
   var body: some View {
@@ -47,19 +49,47 @@ struct PostListView<PostContainer: PostsProvider>: View {
         .padding()
         Spacer()
       }
-      TextField("Search Posts", text: $searchText)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .padding([.bottom, .leading, .trailing])
-      List {
-        ForEach(filteredPosts) { post in
-          PostRowView(post: post, commentsManager: self.commentsManager, debugManager: self.debugManager)
-            .conditionalModifier(post == self.filteredPosts.last, OnAppearModifier {
-              self.postsData.loadPosts()
-            })
+      HStack {
+        TextField("Search Posts", text: $searchText)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+        Button("Sidebar") {
+          withAnimation {
+            self.showSidebar.toggle()
+          }
         }
       }
-      .onAppear {
-        self.postsData.loadPosts()
+      .padding([.bottom, .leading, .trailing])
+      HSplitView {
+        List {
+          ForEach(filteredPosts) { post in
+            PostRowView(post: post, commentsManager: self.commentsManager, debugManager: self.debugManager)
+              .conditionalModifier(post == self.filteredPosts.last, OnAppearModifier {
+                self.postsData.loadPosts()
+                })
+          }
+        }
+        .onAppear {
+          self.postsData.loadPosts()
+        }
+        if postContainer is Subreddit && showSidebar {
+          SidebarView(subreddit: postContainer as! Subreddit)
+            .frame(minWidth: 200, maxWidth: 400)
+        }
+      }
+    }
+  }
+}
+
+struct SidebarView: View {
+  let subreddit: Subreddit
+
+  var body: some View {
+    VStack {
+      if subreddit.headerImg != nil {
+        WebImage(url: subreddit.headerImg!)
+      }
+      ScrollView {
+        Text(subreddit.description)
       }
     }
   }
