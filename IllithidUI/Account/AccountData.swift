1c59c230ae5a4dddef3bc22f407492e98e2ed912
Tyler Gregory <tdgregory@protonmail.com>
2020-04-05 12:49:03 -0500

    Implement account content as a single view that take an AccountContent to determine what content it is showing

--- IllithidUI/Account/AccountData.swift
+++ IllithidUI/Account/AccountData.swift
@@ -1,33 +1,32 @@
 //
 // AccountData.swift
 // Copyright (c) 2020 Flayware
-// Created by Tyler Gregory (@01100010011001010110010101110000) on 4/2/20
+// Created by Tyler Gregory (@01100010011001010110010101110000) on 4/4/20
 //
 
 import Combine
 import os.log
 import SwiftUI
 
+import Alamofire
 import Illithid
 
 class AccountData: ObservableObject {
   @Published private(set) var account: Account?
-  @Published private(set) var overview: [Listing.Content] = []
 
-  @Published private(set) var saved: [Listing.Content] = []
-  @Published private(set) var hidden: [Post] = []
+  @Published private(set) var content: [AccountContent: [Listing.Content]] = [:]
 
-  @Published private(set) var upvoted: [Post] = []
-  @Published private(set) var downvoted: [Post] = []
-
-  @Published private(set) var comments: [Comment] = []
-  @Published private(set) var submissions: [Post] = []
+  private var listingAnchors: [AccountContent: ListingParameters] = [:]
+  private var isExhausted: [AccountContent: Bool] = [:]
 
   private let log = OSLog(subsystem: "com.flayware.IllithidUI.accounts", category: .pointsOfInterest)
 
   init(account: Account?) {
     self.account = account
-    if let account = account { loadAccount(account) }
+    AccountContent.allCases.forEach { type in
+      content[type] = []
+      listingAnchors[type] = ListingParameters()
+    }
   }
 
   convenience init(name: String) {
@@ -36,7 +35,6 @@ class AccountData: ObservableObject {
       switch result {
       case let .success(account):
         self.account = account
-        self.loadAccount(account)
       case let .failure(error):
         self.account = nil
         Illithid.shared.logger.errorMessage("Failure fetching account: \(error)")
@@ -44,79 +42,23 @@ class AccountData: ObservableObject {
     }
   }
 
-  /// Shortcut implementation while working on the view
-  fileprivate func loadAccount(_ account: Account) {
-    let overviewId = OSSignpostID(log: log)
-    let commentsId = OSSignpostID(log: log)
-    let submissionsId = OSSignpostID(log: log)
-
-    os_signpost(.begin, log: log, name: "Load Overview", signpostID: overviewId, "%{public}s", account.name)
-    account.overview { result in
+  func loadContent(content: AccountContent, sort: AccountContentSort, topInterval: TopInterval = .day) {
+    guard !isExhausted[content, default: false] else { return }
+    account?.content(content: content, sort: sort, topInterval: topInterval, parameters: listingAnchors[content]!) { result in
       switch result {
       case let .success(listing):
-        self.overview.append(contentsOf: listing.children)
+        self.content[content]?.append(contentsOf: listing.children)
+        self.listingAnchors[content]?.after = listing.after ?? ""
+        self.isExhausted[content] = listing.after == nil
       case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch overview: \(error)")
+        Illithid.shared.logger.errorMessage("Failed to load \(content) for \(self.account?.name ?? "No account"): \(error)")
       }
-      os_signpost(.end, log: self.log, name: "Load Overview", signpostID: overviewId, "%{public}s", account.name)
     }
+  }
 
-    account.savedContent { result in
-      switch result {
-      case let .success(listing):
-        self.saved.append(contentsOf: listing.children)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch saved content: \(error)")
-      }
-    }
-
-    account.hiddenPosts { result in
-      switch result {
-      case let .success(posts):
-        self.hidden.append(contentsOf: posts)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch hidden posts: \(error)")
-      }
-    }
-
-    account.upvotedPosts { result in
-      switch result {
-      case let .success(posts):
-        self.upvoted.append(contentsOf: posts)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch upvoted posts: \(error)")
-      }
-    }
-
-    account.downvotedPosts { result in
-      switch result {
-      case let .success(posts):
-        self.downvoted.append(contentsOf: posts)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch downvoted posts: \(error)")
-      }
-    }
-
-    os_signpost(.begin, log: log, name: "Load Comments", signpostID: commentsId, "%{public}s", account.name)
-    account.comments { result in
-      switch result {
-      case let .success(comments):
-        self.comments.append(contentsOf: comments)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch comments: \(error)")
-      }
-      os_signpost(.end, log: self.log, name: "Load Comments", signpostID: commentsId, "%{public}s", account.name)
-    }
-
-    os_signpost(.begin, log: log, name: "Load Submissions", signpostID: submissionsId, "%{public}s", account.name)
-    account.submissions { result in
-      switch result {
-      case let .success(posts):
-        self.submissions.append(contentsOf: posts)
-      case let .failure(error):
-        Illithid.shared.logger.errorMessage("Failed to fetch submissions: \(error)")
-      }
-      os_signpost(.end, log: self.log, name: "Load Submissions", signpostID: submissionsId, "%{public}s", account.name)
-    }
+  func clearContent(content: AccountContent) {
+    isExhausted[content] = false
+    listingAnchors[content] = .init()
+    self.content[content]?.removeAll(keepingCapacity: true)
   }
 }
--- IllithidUI/Account/AccountView.swift
+++ IllithidUI/Account/AccountView.swift
@@ -1,20 +1,42 @@
 //
 // AccountView.swift
 // Copyright (c) 2020 Flayware
-// Created by Tyler Gregory (@01100010011001010110010101110000) on 3/21/20
+// Created by Tyler Gregory (@01100010011001010110010101110000) on 4/2/20
 //
 
 import SwiftUI
 
+import Alamofire
 import Illithid
 
-private struct AccountContentView: View {
+private struct Content: View {
   @ObservedObject var data: AccountData
-  let from: KeyPath<AccountData, [Listing.Content]>
+  @ObservedObject var sorter: SortModel<AccountContentSort> = .init(sort: .new, topInterval: .day)
+
+  let content: AccountContent
 
   var body: some View {
-    List(data[keyPath: from]) { item in
-      self.contentView(content: item)
+    List {
+      SortController(model: sorter)
+      ForEach(data.content[content]!) { item in
+        self.contentView(content: item)
+          .onAppear {
+            if item == self.data.content[self.content]!.last {
+              self.data.loadContent(content: self.content, sort: self.sorter.sort, topInterval: self.sorter.topInterval)
+            }
+          }
+      }
+    }
+    .onAppear {
+      self.data.loadContent(content: self.content, sort: self.sorter.sort, topInterval: self.sorter.topInterval)
+    }
+    .onReceive(sorter.$sort) { _ in
+      self.data.clearContent(content: self.content)
+      self.data.loadContent(content: self.content, sort: self.sorter.sort, topInterval: self.sorter.topInterval)
+    }
+    .onReceive(sorter.$topInterval) { _ in
+      self.data.clearContent(content: self.content)
+      self.data.loadContent(content: self.content, sort: self.sorter.sort, topInterval: self.sorter.topInterval)
     }
   }
 
@@ -33,41 +55,20 @@ private struct AccountContentView: View {
   }
 }
 
-private struct AccountCommentsView: View {
-  @ObservedObject var data: AccountData
-
-  var body: some View {
-    List(data.comments) { comment in
-      CommentRowView(comment: comment)
-    }
-  }
-}
-
-private struct AccountPostsView: View {
-  @ObservedObject var data: AccountData
-  let from: KeyPath<AccountData, [Post]>
-
-  var body: some View {
-    List(data[keyPath: from]) { post in
-      PostRowView(post: post)
-    }
-  }
-}
-
 struct AccountView: View {
   @ObservedObject var accountData: AccountData
 
   var body: some View {
     NavigationView {
       List {
-        NavigationLink("Overview", destination: AccountContentView(data: accountData, from: \.overview))
-        NavigationLink("Posts", destination: AccountPostsView(data: accountData, from: \.submissions))
-        NavigationLink("Comments", destination: AccountCommentsView(data: accountData))
+        NavigationLink("Overview", destination: Content(data: self.accountData, content: .overview))
+        NavigationLink("Posts", destination: Content(data: self.accountData, content: .submissions))
+        NavigationLink("Comments", destination: Content(data: self.accountData, content: .comments))
         if accountData.account == Illithid.shared.accountManager.currentAccount {
-          NavigationLink("Saved Items", destination: AccountContentView(data: accountData, from: \.saved))
-          NavigationLink("Hidden", destination: AccountPostsView(data: accountData, from: \.hidden))
-          NavigationLink("Upvoted", destination: AccountPostsView(data: accountData, from: \.upvoted))
-          NavigationLink("Downvoted", destination: AccountPostsView(data: accountData, from: \.downvoted))
+          NavigationLink("Saved Items", destination: Content(data: self.accountData, content: .saved))
+          NavigationLink("Hidden", destination: Content(data: self.accountData, content: .hidden))
+          NavigationLink("Upvoted", destination: Content(data: self.accountData, content: .upvoted))
+          NavigationLink("Downvoted", destination: Content(data: self.accountData, content: .downvoted))
         }
       }
       .listStyle(SidebarListStyle())
