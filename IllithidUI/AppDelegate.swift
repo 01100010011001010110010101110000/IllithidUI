f9ce5ca1df8ec8a8092260ab3cebfec5ae7ab890
Tyler Gregory <tdgregory@protonmail.com>
2020-08-05 00:07:52 -0500

    Add built-in sidebar and toolbar command groups

--- IllithidUI/AppDelegate.swift
+++ IllithidUI/AppDelegate.swift
@@ -1,7 +1,7 @@
 //
 // AppDelegate.swift
 // Copyright (c) 2020 Flayware
-// Created by Tyler Gregory (@01100010011001010110010101110000) on 7/6/20
+// Created by Tyler Gregory (@01100010011001010110010101110000) on 8/1/20
 //
 
 import Cocoa
@@ -53,6 +53,10 @@ struct IllithidApp: App {
             }
           }
         }
+    }
+    .commands {
+      SidebarCommands()
+      ToolbarCommands()
     }
 
     #if os(macOS)
