fcd7e85d5eb5fd1bf3ff7269a963a015ede0db1a
Tyler Gregory <tdgregory@protonmail.com>
2020-08-28 21:26:17 -0500

    Formatting

--- IllithidUI/View Components/Common/IllithidButton.swift
+++ IllithidUI/View Components/Common/IllithidButton.swift
@@ -1,8 +1,16 @@
+// Copyright (C) 2020 Tyler Gregory (@01100010011001010110010101110000)
 //
-// IllithidButton.swift
-// Copyright (c) 2020 Flayware
-// Created by Tyler Gregory (@01100010011001010110010101110000) on 8/1/20
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
 //
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import SwiftUI
 
@@ -68,6 +76,7 @@ private struct MouseView: NSViewRepresentable {
       super.init(frame: .zero)
     }
 
+    @available(*, unavailable)
     required init?(coder _: NSCoder) {
       fatalError("init(coder:) has not been implemented")
     }
